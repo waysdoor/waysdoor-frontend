6ced2c4077efb6cfdad49d3c30b36723bc1f6aa5
g4urav <gaurav.gb.bahuguna@gmail.com>
2024-02-21 14:53:04 +0530

    baseurl

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,6 +1,6 @@
 // config.js
 const API_BASE_URL = process.env.NODE_ENV === 'production'
-    ? 'http://13.236.162.105:8080'
+    ? 'http://13.201.55.215:8080'
     : 'http://localhost:8080';
 
 export default API_BASE_URL;
