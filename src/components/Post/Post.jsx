fe9cce1ad86c5c7d185a94e8156927e8151012d0
g4urav <gaurav.gb.bahuguna@gmail.com>
2024-03-08 13:34:46 +0530

    aws s3 fetch

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -6,24 +6,37 @@ import {useState,useEffect} from 'react'
 import ModeCommentOutlinedIcon from '@mui/icons-material/ModeCommentOutlined'; 
 import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined'; 
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';import { Zoom } from '@mui/material';
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
  function Post({ post }) {
+   const settings = {
+     dots: true,
+     fade: true,
+     infinite: true,
+     speed: 500,
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     waitForAnimate: false
+   };
+   
     const [liked,setLiked]=useState(false)
    const toggleLike=()=>{
     setLiked(!liked)
    } 
 
-     const getImageUrl = () => {
-         if (post.image && post.image.data) {
-             const binaryArray = post.image.data.data;
-             const uint8Array = new Uint8Array(binaryArray);
-             const binaryString = uint8Array.reduce((acc, value) => acc + String.fromCharCode(value), '');
-             const base64String = btoa(binaryString);
-             return `data:image/jpeg;base64,${base64String}`;
-         }
-         return null;
-     };
+    //  const getImageUrl = () => {
+    //      if (post.image && post.image.data) {
+    //          const binaryArray = post.image.data.data;
+    //          const uint8Array = new Uint8Array(binaryArray);
+    //          const binaryString = uint8Array.reduce((acc, value) => acc + String.fromCharCode(value), '');
+    //          const base64String = btoa(binaryString);
+    //          return `data:image/jpeg;base64,${base64String}`;
+    //      }
+    //      return null;
+    //  };
 
- 
+ console.log("posts",post)
      
   return (
       <div  className='p-4 bg-slate-900 text-white  shadow-xl border border-t-2'>
@@ -36,7 +49,14 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';import { Zoo
      
         <div className='w-[70%] bg-black py-10 flex justify-center'>
         <Link  href={`/post/${post._id}`}>  
-        <Image src={getImageUrl()} width={380} height={80} alt='post-image' />
+         
+         <Slider {...settings}>
+           {post.media.map((url,index)=>(
+            <div>
+            <Image key={index} src={url} width={380} height={80} alt='post-image' />
+             </div>
+          ))}
+         </Slider>
         </Link>
         </div>      
    
--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -3,15 +3,7 @@ import React from 'react'
 import PrevButton from '../back-button/PrevButton';
 
  function SinglePost({data}) {
-  const getImageUrl = (binaryData) => {
-    if (binaryData) {
-      const uint8Array = new Uint8Array(binaryData);
-      const binaryString = uint8Array.reduce((acc, value) => acc + String.fromCharCode(value), '');
-      const base64String = btoa(binaryString);
-      return `data:image/jpeg;base64,${base64String}`;
-    }
-    return null;
-  };
+  
   return (
     <div className='p-4 bg-slate-900 text-white  shadow-xl border border-t-2'>
       <PrevButton />
@@ -19,7 +11,7 @@ import PrevButton from '../back-button/PrevButton';
         <span className="text-2xl cursor-pointer text-yellow-400 font-bold">{data.author}</span><span className="mx-2 font-bold">posted on :</span><span className="mx-2 font-bold text-green-500">12 dec 2024</span>
       </div>
       <div className='flex items-center w-2/3 gap-4'>
-        <Image src={getImageUrl(data.image.data.data)} width={80} height={80} alt='post-image'></Image>
+        <Image src={data.media} width={80} height={80} alt='post-image'></Image>
         <p className='text-xl font-semibold '>{data.title}</p>
       </div>
       <div className='py-3 text-white text-normal tracking-wider'>
